78be193919f0faf66a6108541ad5172104766829
Daniel Stankevich <standeo@gmail.com>
2024-07-30 23:23:10 +1000

    improve error reporting

--- parser/parser.go
+++ parser/parser.go
@@ -1,20 +1,27 @@
 package parser
 
 import (
+	"fmt"
 	"monkey/ast"
 	"monkey/lexer"
 	"monkey/token"
+	"strings"
 )
 
 type Parser struct {
 	l         *lexer.Lexer
+	errors    []string // TODO: extend to add row/col
+	progress  []string // literal progress of what is being parsed at the moment
 	currToken token.Token
 	peekToken token.Token
 }
 
 // New creates a new parser given an initialised lexer.
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
 
 	// read two tokens, so currToken and peekToken are both set
 	p.advance()
@@ -23,9 +30,22 @@ func New(l *lexer.Lexer) *Parser {
 	return p
 }
 
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) Progress() string {
+	return strings.Join(p.progress, " ")
+}
+
 func (p *Parser) advance() {
 	p.currToken = p.peekToken
 	p.peekToken = p.l.NextToken()
+	p.progress = append(p.progress, p.currToken.Literal)
+}
+
+func (p *Parser) flushProgress() {
+	p.progress = []string{}
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
@@ -34,6 +54,7 @@ func (p *Parser) ParseProgram() *ast.Program {
 
 	for !p.currTokenIs(token.EOF) {
 		if stmt := p.parseStatement(); stmt != nil {
+			p.flushProgress()
 			program.Statements = append(program.Statements, stmt)
 		}
 		p.advance()
@@ -86,11 +107,17 @@ func (p *Parser) peekTokenIs(t token.TokenType) bool {
 	return p.peekToken.Type == t
 }
 
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be '%s', got '%s' parsing: '%s ...'", t, p.peekToken.Type, p.Progress())
+	p.errors = append(p.errors, msg)
+}
+
 func (p *Parser) advanceIfPeekIs(t token.TokenType) bool {
 	if p.peekTokenIs(t) {
 		p.advance()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
 }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -18,6 +18,8 @@ func TestLetStatements(t *testing.T) {
 	p := New(l)
 
 	program := p.ParseProgram()
+	assertNoErrors(t, p)
+
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
 	}
@@ -50,4 +52,23 @@ func assertLetStatement(t *testing.T, s ast.Statement, name string) {
 
 	test.AssertEqual(t, letStatement.Name.Value, name)
 	test.AssertEqual(t, letStatement.Name.TokenLiteral(), name)
+}
+
+func assertNoErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors:", len(errors))
+	var sep string
+	for i, msg := range errors {
+		if i == len(errors)-1 {
+			sep = "└──"
+		} else {
+			sep = "├──"
+		}
+		t.Errorf("\t%s %s", sep, msg)
+	}
+	t.FailNow()
 }

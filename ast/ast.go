d6865e714cc30e4d52b1c912ae959677e9dc0356
Daniel Stankevich <standeo@gmail.com>
2024-07-31 23:05:51 +1000

    add ast String()

--- ast/ast.go
+++ ast/ast.go
@@ -1,6 +1,9 @@
 package ast
 
-import "monkey/token"
+import (
+	"bytes"
+	"monkey/token"
+)
 
 // Node
 // -----------------------------------------------------------------------------
@@ -8,6 +11,8 @@ import "monkey/token"
 type Node interface {
 	// TokenLiteral is used for debugging and testing.
 	TokenLiteral() string
+	// String will allow us to print AST notes for debugging
+	String() string
 }
 
 // Statement is an identifier and an expression. For example:
@@ -46,7 +51,15 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
-// LetStatement
+func (p *Program) String() string {
+	var out bytes.Buffer
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
+}
+
+// Let Statement
 // -----------------------------------------------------------------------------
 
 type LetStatement struct {
@@ -58,6 +71,22 @@ type LetStatement struct {
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
 // Identifier
 // -----------------------------------------------------------------------------
 
@@ -68,14 +97,47 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string       { return i.Value }
 
-// ReturnStatement
+// Return Statement
 // -----------------------------------------------------------------------------
 
 type ReturnStatement struct {
-	Token token.Token // token.RETURN token
-	Value Expression
+	Token       token.Token // token.RETURN token
+	ReturnValue Expression
 }
 
 func (rs *ReturnStatement) statementNode()       {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+	out.WriteString(";")
+
+	return out.String()
+}
+
+// Expression Statement
+// -----------------------------------------------------------------------------
+
+// ExpressionStatement is a wrapper for expressions so that they could be added
+// to the Statements slice in Program
+type ExpressionStatement struct {
+	Token      token.Token // the first token of the expression
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
